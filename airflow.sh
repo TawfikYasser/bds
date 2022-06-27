068e4a187a0d39268f2350c08fa14babd7eca3af
TawfikYasser <tawfekyassertawfek@gmail.com>
2022-06-28 01:28:06 +0200

    Create airflow.sh

---
+++ airflow.sh
@@ -0,0 +1,4 @@
+# To install [pip needs to be installed]
+pip install "apache-airflow[celery]==2.3.2" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.3.2/constraints-3.7.txt"
+# To run
+python3 -m airflow COMMANDS
